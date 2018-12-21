2ce1d44783c7dbc1e125950ef33f1573e82de1a9
Rodolfo Lopez <rinofitochs@hotmail.com>
2018-12-21 18:26:45 -0500

    ssl smtp

--- contactform/correo.php
+++ contactform/correo.php
@@ -21,8 +21,8 @@ try {
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'escuelanacionalfitness@gmail.com';                 // SMTP username
     $mail->Password = 'Dominiquerr2004****';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom($email,$email);
